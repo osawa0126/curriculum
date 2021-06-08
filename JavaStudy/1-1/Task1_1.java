781b8000684c2a4dd6ef48d7caf8d98c8aa53710
osawa0126 <84456155+osawa0126@users.noreply.github.com>
2021-06-08 21:49:56 +0900

    課題の提出
    Task1_1.java
    Task1_1.class
    の提出

--- JavaStudy/1-1/Task1_1.java
+++ JavaStudy/1-1/Task1_1.java
@@ -12,21 +12,21 @@ public class Task1_1 {
     public static void main(String[] args) {
         
         // ①int型の hogeInt という名前の変数を宣言し、値「50」で初期化
-
+int hogeInt = 50;
 
 
         // ②Stringクラスの hogeString という名前の変数を宣言、値「テスト」で初期化
-
+String hogeString = "テスト";
 
 
         // ③下記の処理について、何をしているのかコメントを記入してください
         // [doubleという型にhogeDouble という名前の変数を宣言し、0.2で初期化]
         double hogeDouble = 0.2;
 
-int hogeInt = 100;
+hogeInt = 100;
         // ④変数 hogeInt を値「100」で上書き
 
-String hogeString = "合格";
+hogeString = "合格";
         // ⑤変数 hogeString を値「合格」で上書き
 
 System.out.println(hogeInt);
